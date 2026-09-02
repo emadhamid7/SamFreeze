7a9e8f4b1199b149d1899ce8cce16fdd1b2e7145
Emadhamid7 <your-email@emadhamid070@gmail.com>
2026-09-02 14:52:34 +0530

    Add Compose Foundation dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -60,6 +60,7 @@ dependencies {
 
     implementation(platform("androidx.compose:compose-bom:2024.06.00"))
     implementation("androidx.compose.ui:ui")
+    implementation("androidx.compose.foundation:foundation")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
